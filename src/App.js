eee7f680fe2c91850e9401f69c16f56bc6e4c260
ferryferdyansyah <ferryferdyansyah002@gmail.com>
2023-01-26 13:49:16 +0700

    membuat web dengan api dari tmdb

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {getMovieList, searchMovie} from './API';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+
+  }, [])
+
+  const PopularMoviesList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+          <div className='Movie-wrapper' key={i}>
+            <div className='title'>{movie.title}</div>
+            <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}></img>
+            <div className='sinopsis'>{movie.overview}</div>
+            <div className='date'>Release: {movie.release_date}</div>
+            <div className='rate'>Rate: {movie.vote_average}</div>
+          </div>
+      )
+    })
+  }
+
+  const search = async(q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ferry's Movie 23</h1>
+        <input 
+        placeholder='Search movie here..' 
+        className='Movie-search'
+        onChange={({target}) => search(target.value)}
+        />
+        <div className='Movie-container'>
+          <PopularMoviesList/>
+        </div>
       </header>
     </div>
   );
